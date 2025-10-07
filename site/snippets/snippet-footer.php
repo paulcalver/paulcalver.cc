c4f5e170c7691bbd18608661f26668d6e1be66bb
paulcalver <paul@paulcalver.cc>
2025-10-07 18:38:08 +0100

    short tag fix

--- site/snippets/snippet-footer.php
+++ site/snippets/snippet-footer.php
@@ -1,10 +1,13 @@
+<?php
+  // site/snippets/snippet-footer.php
+  $assets = $kirby->url('assets');
+?>
+  <script src="<?= $assets ?>/js/vendor.js"></script>
+  <script src="<?= $assets ?>/js/app.min.js"></script>
+
+  <?php if ($site->customEmbeds()->isNotEmpty()): ?>
+    <?= $site->customEmbeds(); ?>
+  <?php endif; ?>
+
 </body>
-
-<script src="<?= $kirby->url('assets') ?>/js/vendor.js"></script>
-<script src="<?= $kirby->url('assets') ?>/js/app.min.js"></script>
-
-<? if($site->customEmbeds()->isNotEmpty()): ?>
-	<?= $site->customEmbeds(); ?>
-<? endif; ?>
-
 </html>
